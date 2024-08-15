9345adec4c8ac2f6a9e4bcf302e9e072121d22cc
Seth Palmer <elderlord30@gmail.com>
2024-08-14 17:05:56 -0700

    Updated and reconfigured

--- lua/settings.lua
+++ lua/settings.lua
@@ -12,33 +12,34 @@ vim.o.mouse          = 'a'
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.o.clipboard      = 'unnamedplus'
+vim.api.nvim_set_option("clipboard", "unnamed")
 
 -- Enable break indent
-vim.o.breakindent    = true
+vim.o.breakindent   = true
 
 -- Save undo history
-vim.o.undofile       = true
+vim.o.undofile      = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase     = true
-vim.o.smartcase      = true
+vim.o.ignorecase    = true
+vim.o.smartcase     = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn    = 'yes'
+vim.wo.signcolumn   = 'yes'
 
 -- Decrease update time
-vim.o.updatetime     = 250
-vim.o.timeoutlen     = 300
+vim.o.updatetime    = 250
+vim.o.timeoutlen    = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt    = 'menuone,noselect'
+vim.o.completeopt   = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors  = true
+vim.o.termguicolors = true
 
 -- Scroll padding
-vim.o.scrolloff      = 8
+vim.o.scrolloff     = 8
 
 -- Tab size
-vim.o.tabstop        = 4
-vim.o.shiftwidth     = 4
+vim.o.tabstop       = 4
+vim.o.shiftwidth    = 4
